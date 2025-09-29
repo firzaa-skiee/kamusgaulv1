43244c1c5ea3ab80d8a7fd18fe3e6ca04770a88f
firzaa-skiee <mfirza.assyauqie@gmail.com>
2025-09-29 20:24:59 +0700

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,12 @@
+import random
+def gen_pass(n) :
+    character =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(n) :
+        password += random.choice(character)
+
+    return password
+
+n = int(input('masukkan panjang password '))
+print('HASIL GENERATE PASSWORD :', gen_pass(n))
